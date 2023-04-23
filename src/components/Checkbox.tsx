3b2c712ee7645078c2b2a1d60178714e6a9f45a0
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-04-22 21:42:01 -0300

    implementando interface de cadastro de habitos

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,36 @@
+import { TouchableOpacity, TouchableOpacityProps, Text, View } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import colors from "tailwindcss/colors";
+
+interface CheckboxProps extends TouchableOpacityProps {
+	title: string;
+	checked?: boolean;
+}
+
+export function Checkbox({ title, checked = false, ...rest }: CheckboxProps) {
+	return (
+		<View>
+			<TouchableOpacity
+				activeOpacity={.7}
+				className="flex-row mb-2 items-center"
+				{...rest}
+			>
+				{ checked ?
+					<View className="h-8 w-8 rounded-lg bg-green-500 items-center justify-center">
+						<Feather
+							name="check"
+							size={20}
+							color={colors.white}
+						/>
+					</View>
+					:
+					<View className="h-8 w-8 rounded-lg bg-zinc-900" />
+				}
+
+				<Text className="text-white text-base ml-3">
+					{title}
+				</Text>
+			</TouchableOpacity>
+		</View>
+	);
+}
